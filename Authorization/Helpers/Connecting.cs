e9ace520ecd7cd309969a0fd46c05432c393f89e
Mike Vazowskii <mikevazowskii@yandex.com>
2024-10-23 18:29:18 +0300

    fuckthis

---
+++ Authorization/Helpers/Connecting.cs
@@ -0,0 +1,14 @@
+﻿using Authorization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization.Helpers
+{
+    internal class Connecting
+    {
+        public static repairEntities conn {  get; set; } = new repairEntities();
+    } 
+}
---
+++ Authorization/Pages/Authentification.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Authorization.Helpers;
+using Authorization.Pages.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authorization.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authentification.xaml
+    /// </summary>
+    public partial class Authentification : Page
+    {
+        private string _txbLogin;
+        private string _psbPassword;
+
+        public Authentification()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Кнопка входа в приложение
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAuth_Click(object sender, RoutedEventArgs e)
+        {
+
+            _txbLogin = TxbLogin.Text;
+            _psbPassword = PsbPassword.Password;
+            // Обращение к БД
+            var data = Connecting.conn.User.FirstOrDefault(x => x.Login == _txbLogin && x.Password == _psbPassword);
+
+            if (data != null)
+            {
+                switch (data.RoleId)
+                {
+                    case 1:
+                        MessageBox.Show($"Здравствуйте {data.Lastname} {data.Firstname} {data.Patronymic}.\nВы вошли как {data.Role.Name}", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                        Navigating.nav.Navigate(new PageManager());
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+
+                    default:
+                        MessageBox.Show("Извините, ваша раоль не определена", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+
+                    
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин и пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+    }
+}
